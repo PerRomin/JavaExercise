743ac07a43c3721d71d36ad9b68857036f2dbb3a
PerRomin <84070399+PerRomin@users.noreply.github.com>
2021-10-12 09:29:18 +0200

    Fist commit
    This file echoes the input word tree times

---
+++ Echo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Echo {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter what word to echo tree times:");
+
+    // String input
+    String name = myObj.nextLine();
+
+    if (name.matches("^.*[^a-zA-Z ].*$") == true){
+	System.out.println("Only letters are allowed");
+    }
+    else{
+	if (name.length() > 15 || name.length() < 1){
+	    System.out.println("wrong length");
+	}
+	else{
+    // Output input by user
+	    System.out.println(name + " " + name + " " + name);
+	}
+    }
+  }
+}
